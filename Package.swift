ba71a80382b747c84e7f5aab285e1625c2cc78e0
Metin Güler <eng.metin@gmail.com>
2024-06-23 16:53:56 +0200

    make functions public for using as swift package

--- Package.swift
+++ Package.swift
@@ -23,7 +23,8 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "kasa",
-            dependencies: []),
+            dependencies: [],
+            path: "Sources"),
         .testTarget(
             name: "kasaTests",
             dependencies: ["kasa"]),
